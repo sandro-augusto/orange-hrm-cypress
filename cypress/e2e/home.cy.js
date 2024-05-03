8c468bb0890e00e884210af50973c0134a8f35fe
Marillia Dantas <marillia.dantas@hotmail.com>
2024-05-03 10:20:21 -0300

    adicionando arquivo de elementos

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -105,15 +105,5 @@ describe('Validar a HomePage ', () => {
                 expect(url).to.eq('https://opensource-demo.orangehrmlive.com/web/index.php/buzz/viewBuzz')
             })
         })
-
-        Context('Pesquisas', () => {
-
-
-
-
-        })
-
-
-
     })
 })
---
+++ cypress/support/elements/adminElements.js
@@ -0,0 +1,7 @@
+export const ADMIN = {
+   inputUsername: '.oxd-input',
+   userRole: 'oxd-select-text-input',
+   inputEmployeeName: 'input[placeholder="Type for hints..."]',
+   inputStatus: 'oxd-select-text-input',
+   buttonSearch: '.oxd-button'
+}
