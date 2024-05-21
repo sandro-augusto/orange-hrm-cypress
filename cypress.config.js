9f10856f0fb54cceb449d34703af57a0212a1a7f
Marillia Dantas <marillia.dantas@hotmail.com>
2024-05-20 23:13:42 -0300

    adding to the main

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("cypress");
-// const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
@@ -11,8 +11,8 @@ module.exports = defineConfig({
     downloads: false,
     experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
-      // allureWriter(on, config);
-      //   return config;
+      allureWriter(on, config);
+        return config;
     },
   },
 });
