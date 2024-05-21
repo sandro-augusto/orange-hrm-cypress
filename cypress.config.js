4ac1fa88bd3729cdff32b0a84782884453b7ec99
Marillia Dantas <marillia.dantas@hotmail.com>
2024-05-20 23:09:00 -0300

    removing unnecessary files

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
